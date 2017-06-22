3588d586bd32bd35179fd866d4c7979198cf39ec
Tania Allard <t.allard@sheffield.ac.uk>
2017-06-22 13:59:06 +0100

    page layout

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -170,3 +170,18 @@ $(function(){
         }
     });
 });
+
+
+/*** page header ***/
+$(document).scroll(function() {
+  navbarScroll();
+});
+
+function navbarScroll() {
+  var y = window.scrollY;
+  if (y > 10) {
+    $('.header').addClass('small');
+  } else if (y < 10) {
+    $('.header').removeClass('small');
+  }
+}
