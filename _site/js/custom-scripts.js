13e09d2217b380964770d0d35ce43d1bf7a1b4c2
Tania Allard <t.allard@sheffield.ac.uk>
2017-06-23 16:57:03 +0100

    started tool bar

--- _site/js/custom-scripts.js
+++ _site/js/custom-scripts.js
@@ -34,3 +34,25 @@ function navbarScroll() {
     $('.header').removeClass('small');
   }
 }
+
+
+$(document).ready(function () {
+
+    var menu = $('.menu');
+    var origOffsetY = menu.offset().top;
+
+    function scroll() {
+        if ($(window).scrollTop() >= origOffsetY) {
+            $('.menu').addClass('sticky');
+            $('.content').addClass('menu-padding');
+        } else {
+            $('.menu').removeClass('sticky');
+            $('.content').removeClass('menu-padding');
+        }
+
+
+    }
+
+    document.onscroll = scroll;
+
+});
